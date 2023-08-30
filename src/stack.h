dd201ef3de55d48eb6e66369542a7634aca3057b
rahul chaudhary <rahul.chaudhary.1711@gmail.com>
2023-08-31 04:53:21 +0530

    updated stack empty check

--- src/stack.h
+++ src/stack.h
@@ -9,7 +9,7 @@ typedef struct StackHead stack_head_t;
 stack_head_t* create();
 stack_node_t* create_node(int);
 void destroy(stack_head_t*);
-bool is_empty(stack_head_t*);
+bool is_stack_empty(stack_head_t*);
 stack_node_t* push(stack_head_t*, int);
 stack_node_t* pop(stack_head_t*);
 #endif /* stack_h */
