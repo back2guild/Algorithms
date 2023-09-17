bb2ffa1722a4553e2ace61742927fdf21ad8c24e
rahul chaudhary <rahul.chaudhary.x64@gmail.com>
2023-09-17 04:45:02 +0000

    queue

--- src/queue.c
+++ src/queue.c
@@ -9,12 +9,44 @@ struct Queue {
 };
 
 Q_t* createQueue(int maxSize) {
-    return NULL;
+    Q_t *q = NULL;
+    assert(maxSize > 0);
+    q = (Q_t*) malloc(sizeof(Q_t));
+    if(q)
+    {
+        q->count = 0;
+        q->front = -1;
+        q->rear = -1;
+        q->maxSize = maxSize;
+        q->qArray = (void**) calloc(q->maxSize, sizeof(void*));
+        if(!q->qArray)
+        {
+            fprintf(stderr, "Queue creation failed");
+            free(q);
+            q = NULL;
+        }
+    }
+    return q;
 }
 
 
 Q_t* destroyQueue(Q_t *q) {
-    return NULL;
+    if(q)
+    {
+        int c = q->front;
+        while(q->count != 0)
+        {
+            free(q->qArray[c++]);
+            q->count--;
+            if(c == q->maxSize)
+                c = 0;
+        }
+        free(q->qArray);
+        q->qArray = NULL;
+        free(q);
+        q = NULL;
+    }
+    return q;
 }
 
 bool enqueue(Q_t *q , void *ptr)
