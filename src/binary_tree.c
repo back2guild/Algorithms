07ce0396381f145f06ad45d7b0074c7e9dd74bc8
rahul chaudhary <rahul.chaudhary.x64@gmail.com>
2023-09-19 16:58:28 +0000

    tree

--- src/binary_tree.c
+++ src/binary_tree.c
@@ -9,6 +9,17 @@ struct NodeElement {
 struct RootElement {
     int size;
     struct NodeElement *root;
-    void (*printer)(void*);
+    void (*printer)(node_t*);
+    int (*compare)(node_t*, node_t*);
 };
 
+
+head_t* createBST(int (*cmp)(node_t*, node_t*), void (*printer)(node_t*)) {
+    head_t *tree = (head_t*) malloc(sizeof(head_t));
+    assert(NULL != tree);
+    tree->root = NULL;
+    tree->size = 0;
+    tree->printer = printer;
+    tree->compare = cmp;
+    return tree;
+}
