5e0ef6e57c533eab9f0e58b4e68686df5fb99fa2
rahul chaudhary <rahul.chaudhary.1711@gmail.com>
2023-09-20 00:26:11 +0530

    joy

--- src/binary_tree.c
+++ src/binary_tree.c
@@ -28,8 +28,29 @@ bool isEmpty(head_t *tree) {
     return (NULL == tree->root && 0 == tree->size);
 }
 
+void _insert(head_t *tree, node_t *root, node_t *node) {
+    if(NULL != tree && NULL != root) {
+        if(tree->compare(root, node) == 0) {
+            
+        }
+        else if(tree->compare(root, node) < 0) {
+            _insert(tree, root->left, node);
+        }
+        else {
+            _insert(tree, root->right, node);
+        }
+    }
+}
+
 bool insertBST(head_t *tree, node_t *node) {
     bool success = false;
-    
+    if(isEmpty(tree)) {
+        tree->root = node;
+    }
+    else {
+        _insert(tree, tree->root, node);
+    }
+    tree->size++;
+    success = true;
     return success;
 }
