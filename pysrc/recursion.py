fa22c05ce19395a3c42ec0e3b2181524a26a89a0
rahul chaudhary <rahul.chaudhary.x64@gmail.com>
2023-10-06 02:33:51 +0530

    stack

--- pysrc/recursion.py
+++ pysrc/recursion.py
@@ -9,6 +9,28 @@ def factorial(num):
         return 1
     else:
         return num * factorial(num - 1)
+    
+
+class Stack(object):
+    def __init__(self) -> None:
+        self.stack = []
+    
+    def push(self,item):
+        self.stack.append(item)
+    
+    def pop(self):
+        if self.empty() is False:
+            return self.stack.pop()
+        else:
+            return True
+    
+    def empty(self):
+        return (len(self.stack) == 0)
+    
+    def size(self):
+        return len(self.stack)
+    
+
 
 """
 def main_factorial(cmdArgs):
